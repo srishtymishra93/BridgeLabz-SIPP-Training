c9d56b5b4d06d983eb307afb69e5699609be90cf
srishtymishra93 <srishtim68@gmail.com>
2025-06-18 10:06:16 +0530

    [srishti mishra]Code of MeanHeight

---
+++ Bridgelabz-SIPP-training/Arrays/MeanHeight.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MeanHeight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0;
+        for (int i = 0; i < 11; i++) {
+            System.out.print("Enter height of player " + (i + 1) + ": ");
+            heights[i] = scanner.nextDouble();
+            sum += heights[i];
+        }
+        double mean = sum / 11;
+        System.out.println("Mean height of the team: " + mean);
+    }
+}
