bac32714b484b7c17bc764f4f30059e1b9b9363c
srishtymishra93 <srishtim68@gmail.com>
2025-06-18 08:37:14 +0530

    code of profitLoss

---
+++ Bridgelabz-SIPP-training/programmingElements/ProfitLoss.java
@@ -0,0 +1,8 @@
+class ProfitLoss{
+	public static void main(String[] args){
+		double profit=191-129;
+	// to find the profit/loss percentage
+		double profit_percentage=(profit/129)*100;
+		System.out.println("The Cost Price is INR 129 and Selling Price is INR 191 \n The Profit is INR "+profit+" and the Profit percentage is "+profit_percentage);
+	}
+}
