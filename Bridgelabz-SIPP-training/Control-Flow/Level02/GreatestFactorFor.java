93e6e95af77597655a301a0c651a4313ed6bab13
srishtymishra93 <srishtim68@gmail.com>
2025-06-18 09:24:29 +0530

    code of greatestFactorFor

---
+++ Bridgelabz-SIPP-training/Control-Flow/Level02/GreatestFactorFor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GreatestFactorFor {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+// Loop
+        int greatestFactor = 1;
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                greatestFactor = i;
+                break;
+            }
+        }
+
+        System.out.println("Greatest factor of " + number + " (besides itself): " + greatestFactor);
+    }
+}
+
