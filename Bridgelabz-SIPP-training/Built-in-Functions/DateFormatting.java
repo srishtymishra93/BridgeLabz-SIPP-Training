92da578d0810a9f8bdb12be873ef21d0640c73ca
srishtymishra93 <srishtim68@gmail.com>
2025-06-20 10:36:14 +0530

    [srishti mishra] DataFormatting

---
+++ Bridgelabz-SIPP-training/Built-in-Functions/DateFormatting.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatting {
+    public static void main(String[] args) {
+        System.out.println("Problem 3: Date Formatting");
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+        System.out.println("dd/MM/yyyy: " + currentDate.format(format1));
+        System.out.println("yyyy-MM-dd: " + currentDate.format(format2));
+        System.out.println("EEE, MMM dd, yyyy: " + currentDate.format(format3));
+    }
+}
