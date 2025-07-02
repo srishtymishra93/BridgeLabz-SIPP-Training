6970a1d3f70eb45adf7da8415bf88d4faf4fd18b
srishtymishra93 <srishtim68@gmail.com>
2025-07-02 09:33:58 +0530

    [srishti mishra] code of ElectricVehicle

---
+++ OOPS/JavaInheritanceSolutions/JavaInheritanceSolutions/HybridInheritance/VehicleManagementSystem/ElectricVehicle.java
@@ -0,0 +1,9 @@
+class ElectricVehicle extends Vehicle {
+    ElectricVehicle(int maxSpeed, String model) {
+        super(maxSpeed, model);
+    }
+
+    void charge() {
+        System.out.println(model + " is charging.");
+    }
+}
