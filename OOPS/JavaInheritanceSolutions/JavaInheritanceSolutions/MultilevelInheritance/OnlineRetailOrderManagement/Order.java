09046d555da164e6448a4028c1ce00d9618379b3
srishtymishra93 <srishtim68@gmail.com>
2025-07-02 09:37:51 +0530

    [srishti mishra] code of order

---
+++ OOPS/JavaInheritanceSolutions/JavaInheritanceSolutions/MultilevelInheritance/OnlineRetailOrderManagement/Order.java
@@ -0,0 +1,41 @@
+class Order {
+    String orderId;
+    String orderDate;
+
+    Order(String orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    String getOrderStatus() {
+        return "Order placed";
+    }
+}
+
+class ShippedOrder extends Order {
+    String trackingNumber;
+
+    ShippedOrder(String orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order shipped with tracking number: " + trackingNumber;
+    }
+}
+
+class DeliveredOrder extends ShippedOrder {
+    String deliveryDate;
+
+    DeliveredOrder(String orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order delivered on: " + deliveryDate;
+    }
+}
