ee0706edacf1c5490d26b683d7c5e040a2613208
srishtymishra93 <srishtim68@gmail.com>
2025-07-02 09:36:40 +0530

    [srishti mishra] code of  Course

---
+++ OOPS/JavaInheritanceSolutions/JavaInheritanceSolutions/MultilevelInheritance/EducationalCourseHierarchy/Course.java
@@ -0,0 +1,35 @@
+class Course {
+    String courseName;
+    int duration; // in hours
+
+    Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
+
+class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+}
+
+class PaidOnlineCourse extends OnlineCourse {
+    double fee;
+    double discount;
+
+    PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    double getDiscountedFee() {
+        return fee - (fee * discount / 100);
+    }
+}
