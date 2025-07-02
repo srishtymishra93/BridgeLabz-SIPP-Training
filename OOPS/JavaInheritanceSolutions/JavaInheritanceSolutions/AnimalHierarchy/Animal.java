992844956b52025b9e7b2ac7a36e9e02cac1f452
srishtymishra93 <srishtim68@gmail.com>
2025-07-02 09:27:18 +0530

    [srishti mishra] code of animal

---
+++ OOPS/JavaInheritanceSolutions/JavaInheritanceSolutions/AnimalHierarchy/Animal.java
@@ -0,0 +1,46 @@
+class Animal {
+    String name;
+    int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Meow! Meow!");
+    }
+}
+
+class Bird extends Animal {
+    Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Chirp! Chirp!");
+    }
+}
