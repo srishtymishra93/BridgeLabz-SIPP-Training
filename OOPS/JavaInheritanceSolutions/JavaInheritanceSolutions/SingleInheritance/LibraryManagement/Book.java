9fa82aea536e98334c2ea1ffe1625c16dee82155
srishtymishra93 <srishtim68@gmail.com>
2025-07-02 09:38:04 +0530

    [srishti mishra] code of Book

---
+++ OOPS/JavaInheritanceSolutions/JavaInheritanceSolutions/SingleInheritance/LibraryManagement/Book.java
@@ -0,0 +1,32 @@
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    void displayInfo() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Author Bio: " + bio);
+    }
+}
