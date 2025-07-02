f087fcab94f3178e3154125cf2a8f85587419b8e
srishtymishra93 <srishtim68@gmail.com>
2025-07-02 09:39:04 +0530

    [srishti mishra] code of Device

---
+++ OOPS/JavaInheritanceSolutions/JavaInheritanceSolutions/SingleInheritance/SmartHomeDevices/Device.java
@@ -0,0 +1,29 @@
+class Device {
+    String deviceId;
+    String status;
+
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+class Thermostat extends Device {
+    double temperatureSetting;
+
+    Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
